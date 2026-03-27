099153aedbe0d166e70b8e0587ec39d23d4aaf80
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-03-27 15:39:53 +0530

    Updated

--- src/O149Leetcode_1_Two_Sum.java
+++ src/O149Leetcode_1_Two_Sum.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class O149Leetcode_1_Two_Sum {
     //Brute Force Approach
@@ -10,10 +12,22 @@ public class O149Leetcode_1_Two_Sum {
         }
         return new int[]{-1,-1};
     }
+    public static int[]optimisedTwoSum(int[]nums,int target){
+        //Note this method
+        //We are doing this in one iteration
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int compliment=target-nums[i];
+            if(map.containsKey(compliment))return new int[]{map.get(compliment),i};
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
 
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
         System.out.println(Arrays.toString(twoSum(nums,target)));
+        System.out.println(Arrays.toString(optimisedTwoSum(nums,target)));
     }
 }
