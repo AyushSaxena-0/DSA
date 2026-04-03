42c202b4d25a3d769942af5f88fd6df3f729f9f3
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-03 12:29:16 +0530

    Updated

--- src/O161Reverse_A_Number.java
+++ src/O161Reverse_A_Number.java
@@ -2,16 +2,22 @@ public class O161Reverse_A_Number {
     //We have used a global static variable 
     static int rev=0;
     static int fun(int n) {
-        if (n % 10 == n) {
-//Note 0%10=0 but it would stop at one digit won't go below
-            return n;
+        if (n == 0) {
+    //Note 0%10=0 but it would stop at one digit won't go below
+            return rev;
         }
         rev= rev*10 + n%10;
         fun(n/10);
         return rev;
     }
+    static int way2(int n,int ans){
+        if(n==0)return ans;
+        return way2(n/10,ans*10+n%10);
+    }
 
     public static void main(String[] args) {
         System.out.println(fun(123456789));
+        System.out.println(way2(123456789,0));
     }
+
 }
