c6bdfd2875f34e796c1db469e9666bf794bda265
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-04 11:26:18 +0530

    Updated

--- src/OOPS_KK/OOPS_2_Class.java
+++ src/OOPS_KK/OOPS_2_Class.java
@@ -4,6 +4,12 @@ package OOPS_KK;
     int rno;
     String name;
     int marks;
+//This is a parameterized constructor
+    Student(int rno,String name,int marks){
+        this.rno=rno;
+        this.name=name;
+        this.marks=marks;
+    }
 }
 
 public class OOPS_2_Class {
@@ -21,12 +27,14 @@ public class OOPS_2_Class {
         std1.name="Ayush";//Variable inside the object is known as instance variable
         std1.marks=99;
         std1.rno=1;
-
+//     LHS during compile time,RHS during runtime
+//     Accessing the memory address of an object is not allowed in java
         Student std2=new Student();
         std2.name="Ram";
         std2.marks=67;
         std2.rno=2;
-        //LHS is declaration RHS is allocation of dynamic memory
+        //LHS is declaration RHS is allocation of dynamic memory and returns reference variable to it
+        //Unlike pointers in C++
         Student std3=new Student();
         std3.name="Shreya";
         std3.marks=90;
@@ -36,5 +44,13 @@ public class OOPS_2_Class {
         System.out.println(std1.rno+" "+std1.name+" "+std1.marks);
         System.out.println(std2.rno+" "+std2.name+" "+std2.marks);
         System.out.println(std3.rno+" "+std3.name+" "+std3.marks);
+        //Constructor is a function in a class
+        //Constructor define what happen when a object is created
+        //It allows us to create new object with all the parameters
+        //Types of Constructor
+        //1)By Default Constructor
+        //2)No Argument Constructor
+        //3)Parameterised Constructor
+        //4)Copy Constructor
     }
 }
