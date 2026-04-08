8c9d9dd3c4d5f663f536cefb3f899f8f5f5f03fe
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-09 00:52:34 +0530

    Updated

--- src/OOPS_KK/OOPS_10_Singleton_Class.java
+++ src/OOPS_KK/OOPS_10_Singleton_Class.java
@@ -3,5 +3,7 @@ package OOPS_KK;
 public class OOPS_10_Singleton_Class {
     private OOPS_10_Singleton_Class(){
         //Now this won't be called in another file
+        //Whatever thing is private would run only in this class
+        int num=33;
     }
 }
