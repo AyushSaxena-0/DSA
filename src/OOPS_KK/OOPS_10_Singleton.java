1bd32748fb7037801d4b1eab8cf2223acfec9a42
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-09 18:44:20 +0530

    Updated

--- src/OOPS_KK/OOPS_10_Singleton.java
+++ src/OOPS_KK/OOPS_10_Singleton.java
@@ -1,13 +1,13 @@
 package OOPS_KK;
 
-public class Singleton {
-    private Singleton(){}
-    private static Singleton instance;
-    public static Singleton getInstance(){
+public class OOPS_10_Singleton {
+    private OOPS_10_Singleton(){}
+    private static OOPS_10_Singleton instance;
+    public static OOPS_10_Singleton getInstance(){
         if(instance==null){
             //Check whether one object is created or not
             //If not create new
-            instance=new Singleton();
+            instance=new OOPS_10_Singleton();
         }
         //Otherwise return old
         return instance;
--- src/OOPS_KK/OOPS_11_Main.java
+++ src/OOPS_KK/OOPS_11_Main.java
@@ -2,10 +2,10 @@ package OOPS_KK;
 
 public class OOPS_11_Main {
     public static void main(String[] args) {
-        Singleton obj=Singleton.getInstance();
-        Singleton obj1=Singleton.getInstance();
-        Singleton obj2=Singleton.getInstance();
-        Singleton obj3=Singleton.getInstance();
+        OOPS_10_Singleton obj= OOPS_10_Singleton.getInstance();
+        OOPS_10_Singleton obj1= OOPS_10_Singleton.getInstance();
+        OOPS_10_Singleton obj2= OOPS_10_Singleton.getInstance();
+        OOPS_10_Singleton obj3= OOPS_10_Singleton.getInstance();
         //All point to same object
         System.out.println(obj1==obj2);
         System.out.println(obj1.equals(obj2));
