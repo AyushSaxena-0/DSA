6e9c13354b6d01120c6822b8a87e8be375e3ceb0
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-10 15:23:24 +0530

    Updated

---
+++ src/O181Recursion_Linear_Search.java
@@ -0,0 +1,21 @@
+public class O181Recursion_Linear_Search {
+    static int helper(int[]arr,int index,int target){
+        if(index==arr.length){
+            return -1;
+        }
+        if(arr[index]==target){
+            return index;
+        }
+        else{
+            return helper(arr,index+1,target);
+        }
+    }
+    static int linearSearch(int[]arr,int target){
+        return helper(arr,0,target);
+    }
+
+    public static void main(String[] args) {
+        int[]arr={5,4,3,1,2,7,8,9};
+        System.out.println(linearSearch(arr,9));
+    }
+}
