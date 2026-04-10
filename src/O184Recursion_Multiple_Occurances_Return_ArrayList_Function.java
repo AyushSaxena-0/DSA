05a7ce51a832680a4c008476476a364ca8df06c4
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-10 20:21:06 +0530

    Updated

---
+++ src/O184Recursion_Multiple_Occurances_Return_ArrayList_Function.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class O184Recursion_Multiple_Occurances_Return_ArrayList_Function {
+    static ArrayList<Integer> findOccurences(int[]arr,int target,int index){
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(index==arr.length){
+            return ans;
+        }
+        if(arr[index]==target){
+            ans.add(index);
+        }
+        ArrayList<Integer>belowCalls = findOccurences(arr,target,index+1);
+        ans.addAll(belowCalls);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[]arr={1,1,1,2,3,4,5,6,7,8,9};
+        System.out.println(findOccurences(arr,1,0));
+    }
+}
