ae2f1721e85faba84cdb5187683035f2889ab25f
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-12 22:19:07 +0530

    Updated

--- src/Dynamic_Programming/DP_1_Basic_Intro.java
+++ src/Dynamic_Programming/DP_1_Basic_Intro.java
@@ -1,6 +1,7 @@
 package Dynamic_Programming;
 
 public class DP_1_Basic_Intro {
+
     //DP stands for Dynamic programming
     //It is a misleading name because mathematics was not considered good at that time
     //They might be murdered if they study maths
@@ -9,4 +10,49 @@ public class DP_1_Basic_Intro {
 
     //Prerequisite
     //Recursion Basics
+    //Arrays
+    //2D Arrays Basics
+    //HashMap Basics
+    //Trees Basics
+    //Dp fixes overlapping SubProblems by storing it in array
+    //So saving the value in array is called Memoisation
+    //Reduces the time complexity from O(2^N) to O(n)
+    //Total function calls which are reduced 2^N-1
+    //Space complexity becomes O(N)
+
+    //Recursion+Memoisation =Top Down dp
+
+    public static int helper(int[]dp,int n){
+        //This is Dp solution
+        if(n<=1)return n;
+        if(dp[n]!=0)return dp[n];
+        dp[n]=helper(dp,n-1)+helper(dp,n-2);
+        return dp[n];
+    }
+    public static int fib(int n) {
+        int[]dp=new int[n+1];
+        return helper(dp,n);
+    }
+    public static int fibrec(int n){
+        if(n<=1)return n;
+        return fibrec(n-1)+fibrec(n-2);
+    }
+    public static int fiboIterative(int n){
+        if(n<=1)return n;
+        int[]dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<dp.length;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        //This is recursive dp (Type 1)
+        System.out.println(fib(40));
+        //This takes so much time by recursion
+        System.out.println(fibrec(40));
+        //This is using Iterative DP (Type 2) Known as bottom up DP
+    }
 }
---
+++ src/Dynamic_Programming/DP_2_Leetcode_746_Minimum_Cost_Climbing_Stairs.java
@@ -0,0 +1,52 @@
+package Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class DP_2_Leetcode_746_Minimum_Cost_Climbing_Stairs {
+    public static int recMinCost(int[]cost, int index){
+        //This is a recursive solution
+        if(index<=1)return cost[index];
+        return cost[index]+Math.min(recMinCost(cost,index-1), recMinCost(cost,index-2));
+    }
+    public static int recSol(int[] cost) {
+        //Top floor is beyond the last stair
+        //We need to be above last stair
+        //This is simple recursive solution
+        int n=cost.length;
+        return Math.min(recMinCost(cost,n-1), recMinCost(cost,n-2));
+    }
+    public static int dpSol(int[]cost, int index, int[]dp){
+        if(index<=1)return cost[index];
+        //We filled array with -1 as default
+        if(dp[index]!=-1)return dp[index];
+        //Saving and returning at the same time
+        return dp[index]=cost[index]+Math.min(dpSol(cost,index-1,dp), dpSol(cost,index-2,dp));
+    }
+    public static int recursiveDp(int[] cost) {
+        //Top floor is beyond the last stair
+        //We need to be above last stair
+        int n=cost.length;
+        int[]dp=new int[n];
+        //What if cost is zero so we cannot use 0 as default look at range of cost
+        Arrays.fill(dp,-1);
+        return Math.min(dpSol(cost,n-1,dp), dpSol(cost,n-2,dp));
+    }
+    public static int iterativeDp(int[] cost) {
+        int n=cost.length;
+        int[]dp=new int[n];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        //Using iterative dp
+        for(int i=2;i<dp.length;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        //This is bottom up dp
+        return Math.min(dp[n-1],dp[n-2]);
+    }
+    public static void main(String[] args) {
+        int[]cost={1,2,3,4,5};
+        System.out.println(recSol(cost));
+        System.out.println(recursiveDp(cost));
+        System.out.println(iterativeDp(cost));
+    }
+}
