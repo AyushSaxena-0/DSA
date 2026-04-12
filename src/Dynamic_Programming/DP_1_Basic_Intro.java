2503726f3c2ddd1eae997c4497ee75c85224ff60
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-12 18:05:49 +0530

    Updated

---
+++ src/Dynamic_Programming/DP_1_Basic_Intro.java
@@ -0,0 +1,12 @@
+package Dynamic_Programming;
+
+public class DP_1_Basic_Intro {
+    //DP stands for Dynamic programming
+    //It is a misleading name because mathematics was not considered good at that time
+    //They might be murdered if they study maths
+    //Hence the misleading name for life safety
+    //DP is Advanced Recursion
+
+    //Prerequisite
+    //Recursion Basics
+}
