fe882e09adb08c0bca74344709e19e6c34ceac06
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-13 19:49:04 +0530

    Updated

---
+++ src/Dynamic_Programming/DP_6_Friends_Pairing_Problem.java
@@ -0,0 +1,5 @@
+package Dynamic_Programming;
+
+public class DP_6_Friends_Pairing_Problem {
+
+}
--- src/O189Merge_Sort.java
+++ src/O189Merge_Sort.java
@@ -1,2 +1,55 @@
+import java.util.Arrays;
+
 public class O189Merge_Sort {
+    public static void mergeSort(int[]nums,int start,int end){
+        //Solving by recursion
+
+        //Stopping condition
+        if(start==end)return;
+
+        //Calculating the mid
+        int mid=start+(end-start)/2;
+
+        //Dividing the array into two parts recursively till size of each is 1
+        mergeSort(nums,start,mid);
+        mergeSort(nums,mid+1,end);
+
+        //Merging the cells
+        merge(nums,start,mid,end);
+    }
+    //We need mid so that we can differentiate both arrays
+    public static void merge(int[]nums,int start,int mid,int end){
+        int[] sorted=new int[end-start+1];
+        //Defining the pointers
+        int i=start;
+        int j=mid+1;
+        int k=0;
+
+        while(i<=mid&&j<=end){
+            if(nums[i]>nums[j]){
+                sorted[k++]=nums[j++];
+            }
+            else if(nums[i]<nums[j]){
+                sorted[k++]=nums[i++];
+            }
+        }
+        //Adding remaining elements of first array if elements were left
+        while(i<=mid){
+            sorted[k++]=nums[i++];
+        }
+        //Adding remaining elements of second array if elements were left
+        while(j<=end){
+            sorted[k++]=nums[j++];
+        }
+        //Copying these elements in the original array at the index used
+        //So that no elements are lost
+        for(int x=0;x<sorted.length;x++){
+            nums[start+x]=sorted[x];
+        }
+    }
+    public static void main(String[] args) {
+        int[]nums={8,7,6,5,4,3,2,1};
+        mergeSort(nums,0,nums.length-1);
+        System.out.println(Arrays.toString(nums));
+    }
 }
