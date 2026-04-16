f0554d572b1b33dbadb7b2c8946b2e122dc8d866
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-16 23:08:16 +0530

    Updated

--- src/O196Recursion_Quick_Sort.java
+++ src/O196Recursion_Quick_Sort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class O196Recursion_Quick_Sort {
-    static void swap(int[]nums,int i,int j){
+      static void swap(int[]nums,int i,int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
@@ -17,12 +17,19 @@ public class O196Recursion_Quick_Sort {
        while(s<=e){
            //Condition is on s and e not start and end
            //so the s and e run till(inclusive) s>e
+           //Note this approach when the cases are many
+           //We use the cases which are simplest
+           //when both start and end violates condition
+           //Swapping them fixes the problem and prevents waste swaps
            while(nums[s]<pivot){
                s++;
            }
            while(nums[e]>pivot){
                e--;
            }
+    //We keep this check because while loop condition would be checked afterwards
+    //There is a possibility of brokerage of condition of while loop
+    //at line number 37 thus we add if(s<=e) at line 35
            if(s<=e){
                swap(nums,s,e);
                s++;
@@ -36,10 +43,18 @@ public class O196Recursion_Quick_Sort {
        quickSort(nums,s,end);
 
     }
-
+    //In internal libraries hybrid Sorting algorithms are used
+    // Which are combination of (Merge sort + Quick Sort)
     public static void main(String[] args) {
         int[]nums={5,4,3,2,1};
         quickSort(nums,0,nums.length-1);
         System.out.println(Arrays.toString(nums));
     }
 }
+//Note the following points about Merge Sort:-
+//Time complexity
+//Worst case O(N^2)
+//Best case O(NLogN)
+//Inplace algorithm
+//Not stable
+//Merge Sort is preferred in Linked List over Quick sort due to memory allocation not continuous
