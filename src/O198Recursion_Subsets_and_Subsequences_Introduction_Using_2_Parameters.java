0645f133ea7f1c2e1aa6b6e7af4fe950c3433e4e
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-24 11:54:21 +0530

    Updated

--- src/O198Recursion_Subsets_and_Subsequences_Introduction_Using_2_Parameters.java
+++ src/O198Recursion_Subsets_and_Subsequences_Introduction_Using_2_Parameters.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class O198Recursion_Subsets_and_Subsequences_Introduction_Using_2_Parameters {
     //Subsets include non-adjacent collection
     //Subset is for array and subsequence is for string
@@ -14,8 +16,25 @@ public class O198Recursion_Subsets_and_Subsequences_Introduction_Using_2_Paramet
         //Skip the element
         subseq(str.substring(1),ans);
     }
+    static ArrayList<String> subseq2(String str, String ans){
+        if(str.isEmpty()){
+            //Since we are not returning anything we are just printing the ans String
+            ArrayList<String>list=new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+        //Pick the element
+        ArrayList<String>left= subseq2(str.substring(1),ans+str.charAt(0));
+        //Skip the element
+        ArrayList<String>right= subseq2(str.substring(1),ans);
+
+        //Adding all the elements in one list and then returning
+        left.addAll(right);
+        return left;
+    }
 
     public static void main(String[] args) {
         subseq("abc","");
+        System.out.println(subseq2("abc",""));
     }
 }
