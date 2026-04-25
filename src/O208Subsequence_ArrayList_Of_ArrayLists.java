c2135bdcd8b8e0e65c207c5c913c160d6416ccb7
AyushSaxena-0 <110738302+AyushSaxena-0@users.noreply.github.com>
2026-04-26 01:15:30 +0530

    Updated

--- src/O208Subsequence_ArrayList_Of_ArrayLists.java
+++ src/O208Subsequence_ArrayList_Of_ArrayLists.java
@@ -14,8 +14,11 @@ public class O208Subsequence_ArrayList_Of_ArrayLists {
                 //Note this way of creating a new arraylist as it creates a clone or copy of orignal arraylist in the outer list
                 //Otherwise we would be modifying the original list places breaking our code
                 ArrayList<String> temp = new ArrayList<>(outer.get(j));
-                //Note we are not taking a loop as all the arraylist before cover all the cases already
-                //Hence don't get confused we are only required to add the string character at the end of each temporary arraylist to add in the outer arraylist
+                // At this point, 'outer' already contains all subsequences formed using
+                // characters before index i (i.e., excluding current character).
+                // For each existing subsequence, we create a new one by including
+                // the current character and add it to the list.
+                // This ensures we generate all combinations with and without the current character.
                 temp.add(str.charAt(i) + "");
                 outer.add(temp);
             }
