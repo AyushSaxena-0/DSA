49b33edede3e1b439a8ca8690219a1dc21d11365
Ayush Saxena <your_github_email@example.com>
2026-06-26 14:02:39 +0530

    Updated

--- src/O47Search_In_2D_Array.java
+++ src/O47Search_In_2D_Array.java
@@ -8,8 +8,8 @@ public class O47Search_In_2D_Array {
         for(int i=0;i<arr.length;i++){
         for(int j=0;j<arr[i].length;j++){
             if(arr[i][j]==target){
-                int[] ans={i,j};
-            return ans;
+
+            return new int[]{i,j};
             }
         }
     }
