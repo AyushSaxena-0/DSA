d6198ac66391ab7c963ef09a473af21438e48f52
Ayush Saxena <your_github_email@example.com>
2026-06-29 11:19:47 +0530

    Updated

--- src/O54Leetcode_34_Find_First_and_Last_Position_Of_Elements_In_Sorted_Array.java
+++ src/O54Leetcode_34_Find_First_and_Last_Position_Of_Elements_In_Sorted_Array.java
@@ -4,21 +4,21 @@ public class O54Leetcode_34_Find_First_and_Last_Position_Of_Elements_In_Sorted_A
     public static int firstoccurance(int[] nums,int target){
         int start=0;
         int end=nums.length-1;
-        int max=-1;
+        int min=-1;
         while(start<=end){
             int mid=start+(end-start)/2;
             if(nums[mid]>target){
                 end=mid-1;
             }
             else if(nums[mid]==target){
-                max=mid;
+                min=mid;
                 end=mid-1;
             }
             else{
                 start=mid+1;
             }
         }
-        return max;
+        return min;
     }
     public static int lastoccurance(int[]nums,int target){
         int start=0;
