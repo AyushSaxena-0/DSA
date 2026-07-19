b1bfd14044e8241a59d7d7659f393642b6bb7593
Ayush Saxena <your_github_email@example.com>
2026-07-19 15:57:15 +0530

    Updated

--- src/O70Leetcode_2974_Minimum_Number_Game.java
+++ src/O70Leetcode_2974_Minimum_Number_Game.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class O70Leetcode_2974_Minimum_Number_Game {
     public static int[] numberGame(int[] nums) {
         Arrays.sort(nums);
+        //Here We have created other
         int[]arr=new int[nums.length];
         for(int i=0;i<nums.length-1;i=i+2){
+            //Alternative Use temp variable in same array to swap nums[i+1] and nums[i] till int i<nums.length-1;
             arr[i+1]=nums[i];
             arr[i]=nums[i+1];
         }
