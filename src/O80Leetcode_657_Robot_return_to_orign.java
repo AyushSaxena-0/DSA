c8962cc53b8036d0233846afc9d32cdcc7715230
Ayush Saxena <your_github_email@example.com>
2026-07-26 22:18:11 +0530

    Updated

--- src/O80Leetcode_657_Robot_return_to_orign.java
+++ src/O80Leetcode_657_Robot_return_to_orign.java
@@ -1,9 +1,11 @@
 public class O80Leetcode_657_Robot_return_to_orign {
     public static boolean judgeCircle(String moves) {
+
         char up='U';
         char down='D';
         char left='L';
         char right='R';
+
         int cup=0;
         int cdown=0;
         int cleft=0;
@@ -26,9 +28,24 @@ public class O80Leetcode_657_Robot_return_to_orign {
         if(cup==cdown && cleft==cright)return true;
         return false;
     }
+    public static boolean judgeCircle2(String moves) {
+
+        int y=0;    //Represents y axis resultant
+        int x=0;    //Represents x axis resultant
+
+        for(char ch:moves.toCharArray()){
+            if(ch=='U')y++;
+            else if(ch=='D')y--;
+            else if(ch=='L')x--;
+            else x++;
+        }
+
+        return x==0&&y==0;
+    }
 
     public static void main(String[] args) {
         String moves="UDUDL";
         System.out.println(judgeCircle(moves));
+        System.out.println(judgeCircle2(moves));
     }
 }
