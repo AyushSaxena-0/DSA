fa859faca6e133add209eb08c97aa6f9c75e5784
Ayush Saxena <your_github_email@example.com>
2026-07-27 21:48:20 +0530

    Updated

--- src/O234Leetcode_1464_Maximum_Product_Of_Two_Elements_In_Array.java
+++ src/O234Leetcode_1464_Maximum_Product_Of_Two_Elements_In_Array.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 public class O234Leetcode_1464_Maximum_Product_Of_Two_Elements_In_Array {
     public static int maxProduct(int[] nums) {
+        //Sorting elements firstly and then using the last two numbers
+        //in sorted array for required product
         Arrays.sort(nums);
         int n=nums.length-1;
         return (nums[n]-1)*(nums[n-1]-1);
