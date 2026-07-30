f15f1270fa0199459a6eae7bef75e57901476b47
Ayush Saxena <your_github_email@example.com>
2026-07-30 21:09:22 +0530

    Updated

--- src/O87Leetcode_3019_Number_of_changing_keys.java
+++ src/O87Leetcode_3019_Number_of_changing_keys.java
@@ -1,13 +1,13 @@
 public class O87Leetcode_3019_Number_of_changing_keys {
     public static int countKeyChanges(String s) {
         int count=0;
-//        char[]keys=s.toLowerCase().toCharArray();
-//        for(int i=1;i<keys.length;i++){
-//            if(keys[i]!=keys[i-1]){
-//                count++;
-//            }
-//Alter avoid creating an array and use toLowerCase() on s itself
-// and save it in s and use s.charAt(i)!=s.charAt(i-1)
+    //        char[]keys=s.toLowerCase().toCharArray();
+    //        for(int i=1;i<keys.length;i++){
+    //            if(keys[i]!=keys[i-1]){
+    //                count++;
+    //            }
+    //Alter avoid creating an array and use toLowerCase() on s itself
+    // and save it in s and use s.charAt(i)!=s.charAt(i-1)
 
         s=s.toLowerCase();
         for(int i=1;i<s.length();i++){
