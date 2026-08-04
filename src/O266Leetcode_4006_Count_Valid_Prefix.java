ee8b4cdd97548024f1ad17145fff248df0191e34
Ayush Saxena <your_github_email@example.com>
2026-08-04 08:14:08 +0530

    Updated

--- src/O266Leetcode_4006_Count_Valid_Prefix.java
+++ src/O266Leetcode_4006_Count_Valid_Prefix.java
@@ -7,6 +7,7 @@ public class O266Leetcode_4006_Count_Valid_Prefix {
         for(int i =0;i<s.length();i++){
             if(s.charAt(i)=='0')zeros++;
             else ones++;
+            //Note This is condition for alternating array
             if(Math.abs(zeros-ones)<2)max++;
         }
         return max;
