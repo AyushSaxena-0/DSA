37914cd1a217d56cc5d9ce509614cba8143b810e
Ayush Saxena <your_github_email@example.com>
2026-08-04 08:08:08 +0530

    Updated

---
+++ src/O266Leetcode_4006_Count_Valid_Prefix.java
@@ -0,0 +1,19 @@
+public class O266Leetcode_4006_Count_Valid_Prefix {
+    public static int countValidPrefixes(String s) {
+        int zeros=0;
+        int ones=0;
+        int max=0;
+
+        for(int i =0;i<s.length();i++){
+            if(s.charAt(i)=='0')zeros++;
+            else ones++;
+            if(Math.abs(zeros-ones)<2)max++;
+        }
+        return max;
+    }
+
+    static void main() {
+        String s = "00110";
+        System.out.println(countValidPrefixes(s));
+    }
+}
