08ee66e0bad8f1c7822ce3ee13c2f584a0126ad4
Ayush Saxena <your_github_email@example.com>
2026-08-06 15:56:07 +0530

    Updated

---
+++ src/O271Leetcode_338_Counting_Bits.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class O271Leetcode_338_Counting_Bits {
+    public static int countones(int a){
+        int count=0;
+        while(a>0){
+            if((a&1)==1){
+                count++;
+            }
+            a=a>>1;
+        }
+        return count;
+    }
+    public static int[] countBits(int n) {
+        //Creating an Array
+        int[]arr=new int[n+1];
+        for(int i=0;i<=n;i++){
+            arr[i]=countones(i);
+        }
+        return arr;
+    }
+
+    static void main() {
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+}
