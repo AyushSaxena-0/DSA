bf723c235a127139d609ec92dc9fe8856ebc9f94
Ayush Saxena <your_github_email@example.com>
2026-08-07 19:46:38 +0530

    Updated

--- src/O271Leetcode_338_Counting_Bits.java
+++ src/O271Leetcode_338_Counting_Bits.java
@@ -19,8 +19,20 @@ public class O271Leetcode_338_Counting_Bits {
         }
         return arr;
     }
+    public static int countBits2(int n){
+        int count=0;
+        while(n>0){
+            n=n&(n-1);
+            //Or do
+            //It removes the rightmost set bit
+            //n=n-(n&-n)
+            count++;
+        }
+        return count;
+    }
 
     static void main() {
         System.out.println(Arrays.toString(countBits(5)));
+        System.out.println(countBits2(5));
     }
 }
