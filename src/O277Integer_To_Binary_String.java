5cc3d8657aa7866469e0784768ab96a6836e408c
Ayush Saxena <your_github_email@example.com>
2026-08-07 19:51:45 +0530

    Updated

---
+++ src/O277Integer_To_Binary_String.java
@@ -0,0 +1,6 @@
+public class O277Integer_To_Binary_String {
+    static void main() {
+        //It returns the String of Binary representation
+        System.out.println(Integer.toBinaryString(10));
+    }
+}
