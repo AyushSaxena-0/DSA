986821996a80de1412e26c8f685ce7e744013a36
Ayush Saxena <your_github_email@example.com>
2026-08-09 22:37:16 +0530

    Updated

--- src/O282Recursion_Basic_Factorial_Of_Number.java
+++ src/O282Recursion_Basic_Factorial_Of_Number.java
@@ -4,8 +4,14 @@ public class O282Recursion_Basic_Factorial_Of_Number {
         if(n<=1)return 1;
         return n*fact(n-1);
     }
+    public static int sum(int n){
+        if(n<=1)return n;
+        return n+sum(n-1);
+    }
     static void main() {
         int x=fact(5);
+        int y=sum(5);
         System.out.println(x);
+        System.out.println(y);
     }
 }
