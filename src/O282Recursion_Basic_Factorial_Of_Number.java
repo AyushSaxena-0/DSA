20d6a8d6dc8782108aee431335abef02a9fb15f6
Ayush Saxena <your_github_email@example.com>
2026-08-09 22:30:56 +0530

    Updated

---
+++ src/O282Recursion_Basic_Factorial_Of_Number.java
@@ -0,0 +1,11 @@
+public class O282Recursion_Basic_Factorial_Of_Number {
+
+    public static int fact(int n){
+        if(n==1)return 1;
+        return n*fact(n-1);
+    }
+    static void main() {
+        int x=fact(10);
+        System.out.println(x);
+    }
+}
