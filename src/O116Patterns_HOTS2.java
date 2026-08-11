073c06c03e635da1435ad2958efe3d61f22c60bd
Ayush Saxena <your_github_email@example.com>
2026-08-11 12:02:25 +0530

    Updated

--- src/O116Patterns_HOTS2.java
+++ src/O116Patterns_HOTS2.java
@@ -59,6 +59,7 @@ public class O116Patterns_HOTS2 {
         for(int[] a:ans){
             System.out.println(Arrays.toString(a));
         }
+        //You can also print the array
     }
 
      */
