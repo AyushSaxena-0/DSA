8bae675325046ec0b30cb4715819f8fe8bda6d11
Ayush Saxena <your_github_email@example.com>
2026-08-12 12:11:15 +0530

    Updated

--- src/O152Leetcode_2946_Matrix_Similarity_After_Cyclic_Shift.java
+++ src/O152Leetcode_2946_Matrix_Similarity_After_Cyclic_Shift.java
@@ -31,6 +31,32 @@ public class O152Leetcode_2946_Matrix_Similarity_After_Cyclic_Shift {
         }
         return Arrays.deepEquals(arr,mat);
     }
+    /*
+    public boolean areSimilar(int[][] mat, int k) {
+        //Outer for loop running k times
+        int[][]test=new int[mat.length][mat[0].length];
+        for(int i=0;i<k;i++){
+            for(int j=0;j<mat.length;j++){
+                for(int l=0;l<mat[j].length;l++){
+                    int n=mat[j].length;
+                    if(j%2==0){
+                        //Shift by one
+                        test[j][Math.floorMod(l-k,n)]=mat[j][l];
+                     //    Note why
+                     //   -1%3=-1
+                     //   but -1 mod 3 is equivalent to 2
+                     //   Math.floorMod(-1,3)==2
+                     //   Hence floor Mod is used
+                    }
+                    else{
+                        test[j][(l+k)%n]=mat[j][l];
+                    }
+                }
+            }
+        }
+        return Arrays.deepEquals(test,mat);
+    }
+     */
 
     public static void main(String[] args) {
         int[][]mat = {{1,2,3},{4,5,6},{7,8,9}};
