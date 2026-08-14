22cc000b24b4a098b4ce59659ffe6c6fce68c8ea
Ayush Saxena <your_github_email@example.com>
2026-08-15 00:38:46 +0530

    Updated

--- src/O224Leetcode_62_Unique_Paths.java
+++ src/O224Leetcode_62_Unique_Paths.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class O224Leetcode_62_Unique_Paths {
     public static int uniquePaths(int m, int n) {
         //I am going to solve it using Dynamic Programming
@@ -19,8 +21,29 @@ public class O224Leetcode_62_Unique_Paths {
         //The last cell gives the value of total ways to reach to that cell
         return dp[m-1][n-1];
     }
+    public static int helper(int m,int n,int row,int col,int[][]dp){
+        if(row==m-1&&col==n-1){
+            return 1;
+        }
+        if(row>m||col>n)return 0;
+        if(dp[row][col]!=-1)return dp[row][col];
+        //once move right
+        int count=0;
+        count+=helper(m,n,row,col+1,dp);
+        //once move down
+        count+=helper(m,n,row+1,col,dp);
+        return dp[row][col]=count;
+    }
+    public static int uniquePaths2(int m, int n) {
+        int[][]dp=new int[m+1][n+1];
+        for(int[]arr:dp){
+            Arrays.fill(arr,-1);
+        }
+        return helper(m,n,0,0,dp);
+    }
 
-    static void main() {
+    public static void main(String[]args) {
         System.out.println(uniquePaths(3, 7));
+        System.out.println(uniquePaths2(3, 7));
     }
 }
