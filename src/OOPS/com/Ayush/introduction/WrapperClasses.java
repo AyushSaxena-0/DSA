d9171ac48177cac8f61ab47a9bf73f570fba31f3
Ayush Saxena <your_github_email@example.com>
2026-08-17 22:46:20 +0530

    Updated

--- src/OOPS/com/Ayush/introduction/WrapperClasses.java
+++ src/OOPS/com/Ayush/introduction/WrapperClasses.java
@@ -1,11 +1,34 @@
 package OOPS.com.Ayush.introduction;
 
 public class WrapperClasses {
+    static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        //Won't work with primitives
+    }
+    static void swap(Integer a,Integer b){
+        Integer temp = a;
+        a = b;
+        b = temp;
+        //Still won't work
+    }
     public static void main(String[] args) {
         int a=10;
-        Integer b=new Integer(10);
+        int b=20;
+        Integer c=new Integer(10);
+        Integer d=new Integer(20);
         //Observe Integer class has much more methods than primitive int
-        System.out.println(b);
-
+        swap(a,b);
+        System.out.println(a+" "+b);
+        //Wrapper class also would not be swapped...But why???
+        System.out.println(c+" "+d);
+        //Because it is a Final class (ctrl+click Integer)
+        //Thus it cannot be modified
+        //Convention is writing final variable with all capital letters
+        final int BONUS=1000;
+        //Won't work as final is used
+        //BONUS=300;
+        //Final variables have to be initialized as they cannot be modified later
     }
 }
