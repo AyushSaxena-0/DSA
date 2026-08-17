92df121fa763c09da570cec5171b838ec99924e2
Ayush Saxena <your_github_email@example.com>
2026-08-17 22:32:42 +0530

    Updated

---
+++ src/OOPS/com/Ayush/introduction/WrapperClasses.java
@@ -0,0 +1,11 @@
+package OOPS.com.Ayush.introduction;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        int a=10;
+        Integer b=new Integer(10);
+        //Observe Integer class has much more methods than primitive int
+        System.out.println(b);
+
+    }
+}
