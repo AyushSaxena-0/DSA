f7bbbd67b8ac535bd320557516ec7c1f2f34867a
Ayush Saxena <your_github_email@example.com>
2026-08-18 16:40:27 +0530

    Updated

---
+++ src/O304Leetcode_324_Wiggle_Sort_II.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class O304Leetcode_324_Wiggle_Sort_II {
+    public static void wiggleSort(int[] nums) {
+        //This problem is trick problem you need to sort it
+        Arrays.sort(nums);
+        int n=nums.length;
+        int x=0;
+        int[]ans=new int[n];
+        int mid=(n+1)/2;
+        int left=mid-1;
+        int right=n-1;
+        //[1,1,1,4,5,6]
+        //      ^     ^
+        //      |     |
+        //    left    end
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                ans[x++]=nums[left--];
+            }
+            else{
+                ans[x++]=nums[right--];
+            }
+        }
+        //Note this copying methord
+        System.arraycopy(ans,0,nums,0,n);
+    }
+
+    public static void main(String[] args) {
+        int[]nums={1,5,1,1,6,4};
+        wiggleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
