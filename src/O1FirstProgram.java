cd01431aa31399764a203c0547b0191671b97fd3
Ayush Saxena <your_github_email@example.com>
2026-08-18 19:09:00 +0530

    Updated

--- src/O1FirstProgram.java
+++ src/O1FirstProgram.java
@@ -3,7 +3,7 @@ public class O1FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Scanner input=new Scanner(System.in);
-        int x= input.nextInt();
+        int x=input.nextInt();
         System.out.println(x);
 
     }
