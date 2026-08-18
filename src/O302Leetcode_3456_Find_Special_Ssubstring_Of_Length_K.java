ab0239d79d0789f8102f4096d73a2cf46227fbae
Ayush Saxena <your_github_email@example.com>
2026-08-18 12:56:05 +0530

    Updated

---
+++ src/O302Leetcode_3456_Find_Special_Ssubstring_Of_Length_K.java
@@ -0,0 +1,34 @@
+public class O302Leetcode_3456_Find_Special_Ssubstring_Of_Length_K {
+    public static boolean hasSpecialSubstring(String s, int k) {
+        //Always remember s.substring(i,j) would have elements form i to j-1
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<=s.length();j++){
+                String ans=s.substring(i,j);
+                //Given condition
+                if(ans.length()!=k)continue;
+                //Given condition
+                if(i>0&&s.charAt(i-1)==s.charAt(i))continue;
+                //Given Condition
+                if(j<s.length()&&s.charAt(j-1)==s.charAt(j))continue;//s.substring(i,j) would have elements form i to j-1
+                //So we are using s.charAt(j-1) to compare with s.charAt(j) as answer has s.substring(i,j)
+                //That means letters from i to j-1 not j
+                boolean same=true;
+                for(int l=0;l<ans.length();l++){
+                    if(ans.charAt(l)!=ans.charAt(0)){
+                        same=false;
+                    }
+                }
+                if(same){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s="aaaaac";
+        int k=5;
+        System.out.println(hasSpecialSubstring(s,k));
+    }
+}
