4e7dd7c3989cc5753518b5f8b5fc431f65f8a05b
Ayush Saxena <your_github_email@example.com>
2026-08-19 19:51:46 +0530

    Updated

--- src/OOPS/Lecture_2/staticExample/MoreStaticConcepts.java
+++ src/OOPS/Lecture_2/staticExample/MoreStaticConcepts.java
@@ -1,30 +1,36 @@
 package OOPS.Lecture_2.staticExample;
 
 public class MoreStaticConcepts {
+
     static void greeting(){
         System.out.println("Hi! Ayush,How are you doing?");
     }
+
     static void fun1(){
         //greeting() can be called directly because greeting is a static method
         //As fun1 is a static method, a non static method cannot be called directly inside a static method
         greeting();
         //Non-static methods are dependent on objects
     }
+
     void fun2(){
         System.out.println("I am a non static method");
         //greeting() is a static method but it is called inside non static function fun2()
         greeting();
     }
+
     static void fun3(){
         //If i want to use a non static method inside a static method
         //Then i need to create object of it
         MoreStaticConcepts obj1=new MoreStaticConcepts();
         obj1.fun2();
     }
+
     void fun4(){
         //Non static method can be called inside non static method
         fun2();
     }
+
     public static void main(String[] args) {
         //Calling a non static method directly won't work because a non static method cannot be called directly inside a static method
         //greeting is static
