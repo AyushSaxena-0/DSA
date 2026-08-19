909ea9ba3a0cd628cd6662f8898f3e7f4159cb97
Ayush Saxena <your_github_email@example.com>
2026-08-20 01:36:10 +0530

    Updated

--- src/OOPS/Lecture_2/singletonClass/Main.java
+++ src/OOPS/Lecture_2/singletonClass/Main.java
@@ -2,6 +2,15 @@ package OOPS.Lecture_2.singletonClass;
 
 public class Main {
     public static void main(String[] args) {
+        //Basically you have a public class and a private constructor
+        //You have a static method to get instance
+        //you have a static class type variable inside the public singleton class
+        //You can only access private methods in that class only so you call the constructor in getinstance
+        //if object doesn't exist you create one and if it does
+        //Don't create return the instance
+        //Its nothing special
+        //Just a definition
+        //Dont worry at all!
         Singleton s1=Singleton.getInstance();
         Singleton s2=Singleton.getInstance();
         Singleton s3=Singleton.getInstance();
