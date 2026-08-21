a61796130e1d000e87caa6d227ab2bbd4109910c
Ayush Saxena <your_github_email@example.com>
2026-08-22 02:21:21 +0530

    Updated

--- src/OOPS/Lecture_3/Inheritance.java
+++ src/OOPS/Lecture_3/Inheritance.java
@@ -4,9 +4,9 @@ public class Inheritance {
     //Inheritance is child class using properties of base class
     //Private members of the parent class are not accessed by the child class
     public static void main(String[] args) {
-//        Box box1 =new Box(10,10,10);
-//        Box box2=new Box(box1);
-//        System.out.println(box2.length+" "+ box2.width+" "+ box2.height);
+        //Box box1 =new Box(10,10,10);
+        //Box box2=new Box(box1);
+        //System.out.println(box2.length+" "+ box2.width+" "+ box2.height);
         BoxWeight bw = new BoxWeight(1,2,3,4);
         System.out.println(bw.height+"   "+bw.length+"  "+"  "+bw.width+"   "+bw.weight);
         //Look BoxWeight inherited the properties,variables and functions of parent Box class
