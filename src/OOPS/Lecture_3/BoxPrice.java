dd8cdfb910bf66697fa48f69b66a17bf215bd1b8
Ayush Saxena <your_github_email@example.com>
2026-08-22 02:13:15 +0530

    Updated

---
+++ src/OOPS/Lecture_3/BoxPrice.java
@@ -0,0 +1,28 @@
+package OOPS.Lecture_3;
+//This is example of Multi Level Inheritance
+
+//          Box
+//           |
+//        BoxWeight
+//           |
+//        BoxPrice
+
+public class BoxPrice extends BoxWeight {
+    double cost;
+
+    public BoxPrice() {
+        this.cost=-1;
+    }
+    public BoxPrice(double cost) {
+        super();
+        this.cost = cost;
+    }
+    public BoxPrice(int length,int width,int height,int weight,double cost) {
+        super(length,width,height,weight);
+        this.cost=cost;
+    }
+    public BoxPrice(BoxPrice other){
+        super(other);
+        this.weight=other.weight;
+    }
+}
