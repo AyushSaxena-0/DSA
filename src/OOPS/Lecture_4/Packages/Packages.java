e867ba6538f84540358bd1ea98b79778269096ab
Ayush Saxena <your_github_email@example.com>
2026-08-22 19:33:29 +0530

    Updated

---
+++ src/OOPS/Lecture_4/Packages/Packages.java
@@ -0,0 +1,16 @@
+package OOPS.Lecture_4.Packages;
+
+public class Packages {
+    //Packages are of two types :-
+
+    //  1)User Defined
+    //  2)In-Built
+
+    //  In Built packages types
+    //  1)lang
+    //  2)io
+    //  3)util
+    //  4)applet    (Don't learn it's obsolete)
+    //  5)awt       (Don't learn it's obsolete)
+    //  6)net
+}
