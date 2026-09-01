00b73fa84146b8e8136bcc50bd19c9d5c409f65f
Ayush Saxena <your_github_email@example.com>
2026-09-01 09:41:29 +0530

    Revised Edition

--- src/LL.java
+++ src/LL.java
@@ -30,9 +30,41 @@ public class LL {
         }
         System.out.print("End");
     }
-
-
-
+    public void addLast(int val){
+        //if the linked list is empty then tail.next would give null pointer exception
+        //So use addfirst function previously created
+        //You can write code again here, but I remembered D.R.Y (Do Not Repeat Yourself)
+        if(tail==null){
+            addFirst(val);
+            return;
+        }
+        Node newNode=new Node(val);
+        tail.next=newNode;
+        tail=newNode;
+        size++;
+    }
+    public void insertAtIndex(int val,int index){
+        if(index==0){
+            addFirst(val);
+            return;
+        }
+        else if(index==size){
+            addLast(val);
+            return;
+        }
+        Node temp=head;
+        //For case when we start from i=0 ,Since inside this node temp=temp.next is happening so we need to run this loop index-2 times (To reach at index-1 position)
+        //For case when we start from i=1 you can run loop till index-1 (i<index)
+        //I find starting from 1 a little more convenient,So I am going start from 1 in linked list Okay !
+        for(int i=1;i<index;i++){
+            //I am reaching the index before where we need to insert
+            temp=temp.next;
+        }
+        //Creating new Node and Doing Necessary things (By drawing on paper)
+        Node newNode=new Node(val);
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
 
 
     private class Node{
--- src/O310Linked_List.java
+++ src/O310Linked_List.java
@@ -1,11 +1,13 @@
 public class O310Linked_List {
     public static void main(String[] args) {
-
         LL list=new LL();
         list.addFirst(4);
         list.addFirst(3);
         list.addFirst(2);
         list.addFirst(1);
+        list.addFirst(0);
+        list.addLast(6);
+        list.insertAtIndex(5,5);
         list.print();
     }
 }
