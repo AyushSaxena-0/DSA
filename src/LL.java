e131cc47afa89dfd684d885a9d2abaff2610dbdc
Ayush Saxena <your_github_email@example.com>
2026-09-01 08:56:54 +0530

    Revised Edition

--- src/LL.java
+++ src/LL.java
@@ -7,6 +7,34 @@ public class LL {
     public LL() {
         this.size=0;
     }
+    public void addFirst(int val){
+        //Draw on paper and then understand
+        //Firstly create a node
+        //We are maintaining head and tail for our convinience
+        Node newNode=new Node(val);
+        newNode.next=head;
+        head=newNode;
+        size++;
+        if(tail==null){
+            tail=newNode;//When newNode is first Node in entire linked list
+        }
+    }
+    public void print(){
+        //You cannot use head for doing the things here
+        //As changing the head changes the linked list itself
+        //So create a new reference variable temp and move ahead as its scope is limited inside the function
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.val+"->");
+            temp=temp.next;
+        }
+        System.out.print("End");
+    }
+
+
+
+
+
     private class Node{
 
         private int val;
--- src/O310Linked_List.java
+++ src/O310Linked_List.java
@@ -2,6 +2,10 @@ public class O310Linked_List {
     public static void main(String[] args) {
 
         LL list=new LL();
-
+        list.addFirst(4);
+        list.addFirst(3);
+        list.addFirst(2);
+        list.addFirst(1);
+        list.print();
     }
 }
