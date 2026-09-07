3a394eb8dbf27855d86dec2e42c90f63341d44e8
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-07 21:29:12 +0530

    Updated my Notes

---
+++ src/O358Stacks.java
@@ -0,0 +1,8 @@
+public class O358Stacks {
+    //In stacks LIFO or FILO
+    //First element to be inserted would be the last element to be out
+    //Last element to be inserted would be the first element to be out
+    //Pop in stack means removing element on top of stacks
+    //Push means inserting elements on top of stack
+
+}
