520c6487b7325f7179791fae8e3fe39ee5095ec3
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-08 13:55:49 +0530

    Updated my Notes

--- src/O368Dynamic_Circular_Queue.java
+++ src/O368Dynamic_Circular_Queue.java
@@ -1,4 +1,9 @@
 public class O368Dynamic_Circular_Queue {
+    //Circular queue has the benefit of reusing the waste storage unlike linear queue
+    //However if the circular queue is full then you cannot insert!
+    //To fix both above problems we have Dynamic Circular Queue
     //[_,_99,100,101] ----> [99,100,101,0,0,0,0,0,0,0,0]
     //Above represents the benefit of having a dynamic circular queue
+    //It uses the empty space and also stores unlimited elements
+
 }
