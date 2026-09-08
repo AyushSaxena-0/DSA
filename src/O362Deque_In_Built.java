3a3337e7d44623ca2f241f1a4c641d7986605d14
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-08 08:17:30 +0530

    Updated my Notes

--- src/O362Deque_In_Built.java
+++ src/O362Deque_In_Built.java
@@ -3,6 +3,7 @@ import java.util.Deque;
 
 public class O362Deque_In_Built {
     //In deque you can add and remove elements from both the sides
+    //It is faster than linked list used in stacks and queue
     public static void main(String[] args) {
         Deque<Integer> deque=new ArrayDeque<>();
         deque.addLast(1);
