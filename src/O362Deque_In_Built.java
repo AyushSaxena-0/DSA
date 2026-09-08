2576d5ecf0888133b0b41c5d9d6db0e7f3d862ca
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-08 08:01:49 +0530

    Updated my Notes

---
+++ src/O362Deque_In_Built.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class O362Deque_In_Built {
+    //In deque you can add and remove elements from both the sides
+    public static void main(String[] args) {
+        Deque<Integer> deque=new ArrayDeque<>();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        deque.addLast(5);
+        deque.addFirst(100);
+        deque.removeFirst();
+        deque.removeLast();
+        deque.addLast(999);
+        System.out.println(deque);
+    }
+}
