de9faacf70cb215896e7f792afe7a767b9f5f383
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-08 09:24:42 +0530

    Updated my Notes

--- src/O358CustomStack.java
+++ src/O358CustomStack.java
@@ -1,8 +1,10 @@
-public class O358Stacks {
+public class O358CustomStack {
     //In stacks LIFO or FILO
     //First element to be inserted would be the last element to be out
     //Last element to be inserted would be the first element to be out
     //Pop in stack means removing element on top of stacks
     //Push means inserting elements on top of stack
+    public static void main(String[] args) {
 
+    }
 }
---
+++ src/O359CustomQueue.java
@@ -0,0 +1,3 @@
+public class O359CustomQueue {
+
+}
