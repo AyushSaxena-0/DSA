9fa4b478e98638889f1c0b21c769b9df71b4b720
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-09 11:55:41 +0530

    Updated my Notes

---
+++ src/MyQueue.java
@@ -0,0 +1,61 @@
+import java.util.Stack;
+
+class MyQueue {
+    //You need to declare the variable of class first
+    //Only then you can think of assigning and initializing them in constructor 
+    //LeetCode 232 Implement Queue using stacks
+
+    public Stack<Integer> stack1;
+    public Stack<Integer>stack2;
+
+    public MyQueue() {
+        stack1=new Stack<>();
+        stack2=new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        //This is diffrent
+        //Empty stack 1 into stack 2
+        while(!stack1.isEmpty()){
+            int x=stack1.pop();
+            stack2.push(x);
+        }
+        int ans=stack2.pop();
+        while(!stack2.isEmpty()){
+            int x=stack2.pop();
+            stack1.push(x);
+        }
+        return ans;
+    }
+
+    public int peek() {
+        while(!stack1.isEmpty()){
+            int x=stack1.pop();
+            stack2.push(x);
+        }
+        int ans=stack2.peek();
+        while(!stack2.isEmpty()){
+            int x=stack2.pop();
+            stack1.push(x);
+        }
+        return ans;
+    }
+
+
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
