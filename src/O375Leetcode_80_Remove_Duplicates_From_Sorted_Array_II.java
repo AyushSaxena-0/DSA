2a32a4fbcf4a3dc24b3c0da8d6914e2652c5b22a
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-10 18:05:11 +0530

    Updated my Notes

--- src/O375Leetcode_80_Remove_Duplicates_From_Sorted_Array_II.java
+++ src/O375Leetcode_80_Remove_Duplicates_From_Sorted_Array_II.java
@@ -2,9 +2,12 @@ import java.util.Arrays;
 
 public class O375Leetcode_80_Remove_Duplicates_From_Sorted_Array_II {
     public static int removeDuplicates(int[] nums) {
-        int k=2;
-        for(int i=2;i<nums.length;i++){
-            if(nums[i]!=nums[k-2]){
+        //Really love this approach
+        //The i pointer moves in array from start to end
+        //But k moves with almost two command
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(k<2||nums[i]!=nums[k-2]){
                 nums[k++]=nums[i];
             }
         }
