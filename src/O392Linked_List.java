3cdff15e5b7edff33e999d0b253de6788792693e
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-16 01:12:49 +0530

    Updated my Notes

---
+++ src/O392Linked_List.java
@@ -0,0 +1,7 @@
+public class O392Linked_List {
+    //This topic was already covered by kunal so I didn't made New java classes
+    public static void main(String[] args) {
+        System.out.println("Linked List Questions are mostly solved my slow and fast pointers");
+        System.out.println("Linked list include questions of Cycle detection and Middle element");
+    }
+}
