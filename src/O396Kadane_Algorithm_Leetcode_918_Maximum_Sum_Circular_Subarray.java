9123d1af839c3e3a0ba6ce5e58a440f761f181f3
Ayush Saxena <ayush.saxena0412@gmail.com>
2026-09-18 23:30:49 +0530

    Updated my Notes

---
+++ src/O396Kadane_Algorithm_Leetcode_918_Maximum_Sum_Circular_Subarray.java
@@ -0,0 +1,27 @@
+public class O396Kadane_Algorithm_Leetcode_918_Maximum_Sum_Circular_Subarray {
+    public static int maxSubarraySumCircular(int[] nums) {
+        //Note either the max sum would be circular or min sum
+        //If max sum is circular then min sum is straight
+        //If min sum is circular then max sum is straight
+        //Calculate max of max sum and sum of (array-min)
+        int max=nums[0];
+        int min=nums[0];
+        int sum=nums[0];
+        int bestx=nums[0];
+        int bestn=nums[0];
+        for(int i=1;i<nums.length;i++){
+            max=Math.max(max+nums[i],nums[i]);
+            bestx=Math.max(bestx,max);
+            min=Math.min(min+nums[i],nums[i]);
+            bestn=Math.min(bestn,min);
+            sum+=nums[i];
+        }
+        if(bestx<0)return bestx;
+        return Math.max(bestx,sum-bestn);
+    }
+
+    public static void main(String[] args) {
+        int[]nums={5,-4,-5,-6,5};
+        System.out.println(maxSubarraySumCircular(nums));
+    }
+}
